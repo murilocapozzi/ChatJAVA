6e4b764991577ee2b75968a341e0551aa8c10ec4
Pedro Pinheiro <pepeaugust.10@gmail.com>
2023-09-11 17:30:34 -0300

    Identificação dos clientes
    Implementação apenas visual, para auxiliar estudo em aula, não altera lógica de comunicação cliente/servidor

--- Client.java
+++ Client.java
@@ -1,11 +1,10 @@
-package entities;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import java.lang.String;
 
 public class Client implements Runnable {
 	
@@ -24,32 +23,34 @@ public class Client implements Runnable {
 		String str;
 		while((str = client_socket.getMessage()) != null) {
 
-			System.out.println("Mensagem: " + str);
+			System.out.println(str);
 			
 		}
 	}
 	
 	/* Registra o cliente, podendo escrever e ler mensagens vindas de outros */
 	public void start() throws UnknownHostException, IOException {
+		String usr;
+		System.out.print("Insira seu usuario: ");
+		usr = scanner.nextLine();
 		client_socket = new ClientSocket();
-		
 		/* Receber o fluxo de caracteres */
 		System.out.println("Cliente conectado ao servidor em " + client_socket.IP + ":" + Server.PORT);
 		
 		new Thread(this).start(); /* Thread que irá receber as mensagens dos outros clientes antes mesmo que este mande uma mensagem */
-		envioMensagem();
+		envioMensagem(usr);
 	}
 	
 	/* Lê do terminal pelo Scanner(System.in) e faz o fluxo da mensagem para o servidor */
-	private void envioMensagem() throws IOException {
+	private void envioMensagem(String usr) throws IOException {
 		String str;
 		do {
 			System.out.println("Digite uma mensagem ou /exit para finalizar:");
 			str = scanner.nextLine();
-
+			str = usr + ": " + str;
 			client_socket.sendMessage(str);			
 			
-		} while(!str.equalsIgnoreCase("/exit"));
+		} while(!str.equalsIgnoreCase(usr + ": /exit"));
 	}
 	
 	
--- ClientSocket.java
+++ ClientSocket.java
@@ -1,5 +1,3 @@
-package entities;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.net.Socket;
